b884f8f61241972bfc9e288079bcb9ebeee8b0c9
Jack'lul <jacklulcat@gmail.com>
2025-02-19 19:38:14 +0100

    Correct BRANCH and TAG in other places too

--- scripts/version.sh
+++ scripts/version.sh
@@ -10,8 +10,16 @@ fi
 
 REPOSITORY="$(realpath "$REPOSITORY")"
 
-echo "TAG=$(git -C "$REPOSITORY" describe --tags --always 2>/dev/null)" > "$REPOSITORY/.version"
-echo "BRANCH=$(git -C "$REPOSITORY" rev-parse --abbrev-ref HEAD)" >> "$REPOSITORY/.version"
+if [[ "$GITHUB_REF" == refs/tags/* ]] || [[ "$GITHUB_REF" == "refs/heads/master" ]]; then
+    GIT_TAG=$(git -C "$REPOSITORY" describe --tags --abbrev=0 2> /dev/null || echo "")
+fi
+
+#shellcheck disable=SC2015
+echo "TAG=$([ -n "$GIT_TAG" ] && echo "$GIT_TAG" || git -C "$REPOSITORY" describe --tags --always 2>/dev/null)" > "$REPOSITORY/.version"
+
+#shellcheck disable=SC2015
+echo "BRANCH=$([ -n "$GIT_TAG" ] && echo "master" || git -C "$REPOSITORY" rev-parse --abbrev-ref HEAD)" >> "$REPOSITORY/.version"
+
 echo "HASH=$(git -C "$REPOSITORY" rev-parse --short=8 HEAD)" >> "$REPOSITORY/.version"
 
 cat "$REPOSITORY/.version"
