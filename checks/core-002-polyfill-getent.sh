7d880686207edfbee0045778464c73d1294ed5b5
Jack'lul <jacklulcat@gmail.com>
2025-08-16 08:48:03 +0200

    Do not fail if strings we are looking for are missing

--- checks/core-002-polyfill-getent.sh
+++ checks/core-002-polyfill-getent.sh
@@ -4,9 +4,6 @@
 #shellcheck disable=SC1091
 . "$(dirname "$(readlink -f "$0")")/common-core.sh"
 
-find1="getent hosts"
-found1=false
-
 [ -z "$FILES" ] && exit 1
 for file in $FILES; do
     { [ ! -f "$file" ] || { [ "$(head -c 3 "$file")" != "#!/" ] && ! grep -q '; then' "$file" ; } ; } && continue
@@ -16,18 +13,11 @@ for file in $FILES; do
     # No comments
     contents="$(grep -o "^[^#]*" < "$file"))"
 
-    # Checks
-    if echo "$contents" | grep -qaE "$find1"; then
-        found1=true
-        contents="$(echo "$contents" | grep -aFv "$find1")" # /gravity.sh
-    fi
+    # Exceptions (@TODO the way these are handled needs to be improved)
+    contents="$(echo "$contents" | grep -aFv "getent hosts")" # /gravity.sh
 
+    # Checks
     echo "$contents" | grep -aEn "getent [[:alnum:]_]+ " && exit 1
 done
-
-if [ "$found1" = false ]; then
-    echo "'$find1' = $found1"
-    exit 1
-fi
 
 exit 0
--- checks/core-002-polyfill-tput.sh
+++ checks/core-002-polyfill-tput.sh
@@ -4,13 +4,6 @@
 #shellcheck disable=SC1091
 . "$(dirname "$(readlink -f "$0")")/common-core.sh"
 
-find1="tput colors"
-find2="tput civis"
-find3="tput cnorm"
-found1=false
-found2=false
-found3=false
-
 [ -z "$FILES" ] && exit 1
 for file in $FILES; do
     { [ ! -f "$file" ] || { [ "$(head -c 3 "$file")" != "#!/" ] && ! grep -q '; then' "$file" ; } ; } && continue
@@ -20,30 +13,13 @@ for file in $FILES; do
     # No comments
     contents="$(grep -o "^[^#]*" < "$file"))"
 
+    # Exceptions (@TODO the way these are handled needs to be improved)
+    contents="$(echo "$contents" | grep -aFv "tput colors")" # /advanced/Scripts/COL_TABLE
+    contents="$(echo "$contents" | grep -aFv "tput civis")" # /advanced/Scripts/piholeDebug.sh
+    contents="$(echo "$contents" | grep -aFv "tput cnorm")" # /advanced/Scripts/piholeDebug.sh
+
     # Checks
-    if echo "$contents" | grep -qaE "$find1"; then
-        found1=true
-        contents="$(echo "$contents" | grep -aFv "$find1")" # /advanced/Scripts/COL_TABLE
-    fi
-
-    if echo "$contents" | grep -qaE "$find2"; then
-        found2=true
-        contents="$(echo "$contents" | grep -aFv "$find2")" # /advanced/Scripts/piholeDebug.sh
-    fi
-
-    if echo "$contents" | grep -qaE "$find3"; then
-        found3=true
-        contents="$(echo "$contents" | grep -aFv "$find3")" # /advanced/Scripts/piholeDebug.sh
-    fi
-
     echo "$contents" | grep -aEn "\s+tput [[:alnum:]_]+" && exit 1
 done
-
-if [ "$found1" = false ] || [ "$found2" = false ] || [ "$found3" = false ]; then
-    echo "'$find1' = $found1"
-    echo "'$find2' = $found2"
-    echo "'$find3' = $found3"
-    exit 1
-fi
 
 exit 0
