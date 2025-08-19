d5d476dc2abf7cb523eeb6be4ad1720f9fd06fc8
Jack'lul <jacklulcat@gmail.com>
2025-08-19 17:20:47 +0200

    Update rc.sh

--- files/opt/share/pihole/rc.sh
+++ files/opt/share/pihole/rc.sh
@@ -28,17 +28,22 @@ case $1 in
                 fi
             fi
 
+            if [ -z "$(id -u pihole 2> /dev/null)" ]; then
+                echo "Warning: User 'pihole' does not exist!" >&2
+            fi
+
             # Check if we can start the daemon the intended way
             if
                 [ -n "$run_as_user" ] && \
                 setcap CAP_NET_BIND_SERVICE,CAP_NET_RAW,CAP_NET_ADMIN,CAP_SYS_NICE,CAP_IPC_LOCK,CAP_CHOWN+eip "/opt/bin/pihole-FTL"
             then
                 PREARGS="nonroot $run_as_user"
-                ARGS="-- -u $run_as_user -g $run_as_user" # even when user will be 'pihole' we need to specify this, for some reason
 
-                #if [ "$run_as_user" != "pihole" ]; then # without this it will try to use hardcoded 'pihole' user
-                #    ARGS="-- -u $run_as_user -g $run_as_user"
-                #fi
+                # Explicitly specify user and group for the dnsmasq to use
+                # Without this dnsmasq will fail with error 'unknown user or group: root' if the root user is not called 'root'
+                if [ "$run_as_user" != "pihole" ] || [ "$root_user" != "root" ]; then
+                    ARGS="-- -u $run_as_user -g $run_as_user"
+                fi
             else
                 echo "Warning: Starting in an unsupported way - expect issues to happen!" >&2
 
@@ -47,9 +52,9 @@ case $1 in
 
                     echo "Warning: Starting pihole-FTL as '$root_user' (then changing to '$run_as_user') because setting capabilities is not supported on this system" >&2
                 else
-                    ARGS="-- -u $root_user -g $root_group" # workaround for "[dnsmasq] unknown user or group: root" error
+                    ARGS="-- -u $root_user -g $root_group" # still needed to avoid dnsmasq error mentiond above
 
-                    echo "Warning: Starting pihole-FTL as '$root_user' because couldn't find suitable user to run as" >&2
+                    echo "Warning: Starting pihole-FTL as '$root_user' because couldn't find a suitable user to run as" >&2
                 fi
             fi
         fi
